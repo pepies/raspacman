e84ef258014cf1d3b3830eaf8568806041050ca2
Peter Brecska <pbrecska@gmail.com>
2018-10-12 14:43:10 +0200

    bar

--- back/src/Api/create.php
+++ back/src/Api/create.php
@@ -1,5 +1,9 @@
 <?php
 
+header("Access-Control-Allow-Headers: Content-Type,Authorization");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+header("Access-Control-Allow-Origin: *");
+
 include __DIR__."/../../bootstrap.php";
 
 use \rpman\Models\HttpRequest;
--- back/src/Models/HttpRequest.php
+++ back/src/Models/HttpRequest.php
@@ -14,9 +14,6 @@ class HttpRequest
 
     public function __construct()
     {
-        header("Access-Control-Allow-Headers: Content-Type,Authorization");
-        header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
-        header("Access-Control-Allow-Origin: *");
         $this->setContent();
     }
 
