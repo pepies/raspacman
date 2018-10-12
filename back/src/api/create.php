6e6b04d045b2562aa91c7d5c043637511e8bf967
Peter Brecska <pbrecska@gmail.com>
2018-10-12 20:41:27 +0200

    lets test parser

--- back/src/api/create.php
+++ back/src/api/create.php
@@ -4,13 +4,15 @@ include __DIR__."/../../bootstrap.php";
 
 use \rpman\Models\HttpRequest;
 use \rpman\Models\Storage;
+use \rpman\Models\Parse;
 
 //get http data from post content
 $http = new HttpRequest();
 $levelArray = $http->getContent();
 
 //parse array to Level object
-
+$Array = new Parse($levelArray);
+// $Array->toLevelObject();
 //store it
 // $storage = new Storage();
 // $storage->store($level);
