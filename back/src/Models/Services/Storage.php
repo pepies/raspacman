2c165b6fa403830f191226d6d1db3990d3f1d315
Peter Brecska <pbrecska@gmail.com>
2018-10-15 18:33:00 +0200

    bug - bad Entities path in EntityManager config

--- back/src/Models/Services/Storage.php
+++ back/src/Models/Services/Storage.php
@@ -13,7 +13,7 @@ class Storage
 
     public function __construct()
     {
-        $paths = array(__DIR__."../Entities");
+        $paths = array(__DIR__."/../Entities");
         $isDevMode = true;
         $config = Setup::createAnnotationMetadataConfiguration(
             $paths,
