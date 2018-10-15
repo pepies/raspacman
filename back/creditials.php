c2c0dca5c063f3e9845fd96f99e5692390616c87
Peter Brecska <pbrecska@gmail.com>
2018-10-15 19:13:52 +0200

    daamn, wrong dir. so no gitignore

--- back/creditials.php
+++
@@ -1,7 +0,0 @@
-<?php
-$path = __DIR__ . '/db.sqlite';
-define('PATH', $path);
-define('PASS', 'blneZdTWD1');
-define('LOGIN', 'xendh224');
-define('DBNAME', 'xendh224');
-define('HOST', 'mariadb101.websupport.sk:3312');
